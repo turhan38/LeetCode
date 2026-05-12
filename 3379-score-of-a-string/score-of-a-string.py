02d639425f0ff8f5ff7b0259b7b8ac0df1f73baa
İbrahim Turhan Bayraktar <50884759+turhan38@users.noreply.github.com>
2026-05-12 11:26:28 +0300

    Time: 3 ms (34.38%) | Memory: 12.3 MB (89.64%) - LeetSync

---
+++ 3379-score-of-a-string/score-of-a-string.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def scoreOfString(self, s):
+        total =0
+        for i in range(len(s)-1):
+            total+=abs(ord(s[i])-ord(s[i+1]))
+        return total
+    
+        
