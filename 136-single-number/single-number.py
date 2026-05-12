1e194b80b04d14552c62299e357c1955f3968e76
İbrahim Turhan Bayraktar <50884759+turhan38@users.noreply.github.com>
2026-05-12 10:52:20 +0300

    Time: 15 ms (22.66%) | Memory: 13.9 MB (78.74%) - LeetSync

---
+++ 136-single-number/single-number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        nums.sort()
+        i,lNums =0, len(nums)
+        while i<lNums:
+            if(i == lNums-1 or nums[i]!=nums[i+1]):
+                return nums[i]
+            else:
+                i+=2
+            
+        
